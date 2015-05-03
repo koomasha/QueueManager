6fce5afcb76fa3fada23f69dc2730bac336dfbe6
koomasha <koomasha@gmail.com>
2015-05-03 08:09:09 +0300

    route

--- client/router.js
+++ client/router.js
@@ -1,29 +1,34 @@
-  if (Meteor.isCordova) {
-	  this.route('mobile',{
-		  path:'/',
-		  layoutTemplate: 'mobilelayout',
-		  data: {ismain:true}
-	});
-  }
-  else{
-	Router.map(function () {
-	  this.route('home',{
+Router.map(function () {
+
+  	if (Meteor.isCordova) 
+  	{
+	  	this.route('mobile',{
+		  	path:'/',
+		  	layoutTemplate: 'mobilelayout',
+		  	data: {ismain:true}
+		});
+  	}
+  	else
+  	{
+	  	this.route('home',{
 			path:'/',
 			layoutTemplate: 'homelayout'
 		});
-	  this.route('bo',{
-			path:'/bo',
-			layoutTemplate: 'bolayout',
-		});
-		this.route('dashboard',{
-		  path:'/dashboard',
-		  layoutTemplate: 'bolayout',
-		});
-	  this.route('mobile',{
+	  	this.route('mobile',{
 			path:'/mobile',
 			layoutTemplate: 'mobilelayout',
 			data: {ismain:true}
 		});
+	}
+
+	this.route('bo',{
+		path:'/bo',
+		layoutTemplate: 'bolayout',
 	});
-}
+	this.route('dashboard',{
+	  	path:'/dashboard',
+	  	layoutTemplate: 'bolayout',
+	});
+	  
+});
 
