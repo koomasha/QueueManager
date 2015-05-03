75c30cd7403bd717e0e27ec83ea44d0796571ede
koomasha <koomasha@gmail.com>
2015-05-03 07:40:47 +0300

    w

--- client/router.js
+++ client/router.js
@@ -1,36 +1,29 @@
   if (Meteor.isCordova) {
-  this.route('mobile',{
-      path:'/',
-      layoutTemplate: 'mobilelayout',
-      data: {ismain:true}
-    });
-
+	  this.route('mobile',{
+		  path:'/',
+		  layoutTemplate: 'mobilelayout',
+		  data: {ismain:true}
+	});
   }
-  else
-  {
-
-
-Router.map(function () {
-  this.route('home',{
-  		path:'/',
-  		layoutTemplate: 'homelayout'
-  	});
-  this.route('bo',{
-  		path:'/bo',
-  		layoutTemplate: 'bolayout',
-  	});
-    this.route('dashboard',{
-      path:'/dashboard',
-      layoutTemplate: 'bolayout',
-    });
-  this.route('mobile',{
-  		path:'/mobile',
-  		layoutTemplate: 'mobilelayout',
-  		data: {ismain:true}
-  	});
-});
-<<<<<<< HEAD
+  else{
+	Router.map(function () {
+	  this.route('home',{
+			path:'/',
+			layoutTemplate: 'homelayout'
+		});
+	  this.route('bo',{
+			path:'/bo',
+			layoutTemplate: 'bolayout',
+		});
+		this.route('dashboard',{
+		  path:'/dashboard',
+		  layoutTemplate: 'bolayout',
+		});
+	  this.route('mobile',{
+			path:'/mobile',
+			layoutTemplate: 'mobilelayout',
+			data: {ismain:true}
+		});
+	});
 }
-=======
-}
->>>>>>> origin/master
+
