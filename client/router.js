34f4b0b2cead7db1c2e367a944c1f214d9e71cd8
koomasha <koomasha@users.noreply.github.com>
2015-05-03 07:19:04 +0300

    Update router.js

--- client/router.js
+++ client/router.js
@@ -1,3 +1,15 @@
+  if (Meteor.isCordova) {
+  this.route('mobile',{
+      path:'/',
+      layoutTemplate: 'mobilelayout',
+      data: {ismain:true}
+    });
+
+  }
+  else
+  {
+
+
 Router.map(function () {
   this.route('home',{
   		path:'/',
@@ -7,17 +19,14 @@ Router.map(function () {
   		path:'/bo',
   		layoutTemplate: 'bolayout',
   	});
-  this.route('dashboard',{
+    this.route('dashboard',{
       path:'/dashboard',
       layoutTemplate: 'bolayout',
     });
-  
   this.route('mobile',{
   		path:'/mobile',
   		layoutTemplate: 'mobilelayout',
   		data: {ismain:true}
   	});
 });
-
-
-dashboard
+}
