5cffd44cc5ce613e4a8ddfe2368fcf81e7c277f2
koomasha <koomasha@gmail.com>
2015-05-04 07:56:14 +0300

    new

--- client/bo/dashboard.js
+++ client/bo/dashboard.js
@@ -1,4 +1,22 @@
-/*Login*/
+/*//////////////////////////////
+     Sesion Settings
+/////////////////////////////*/
+	Session.setDefault('showBoAddBranch',false);
+	Session.setDefault('branchId',null);
+
+/*//////////////////////////////
+     LAYOUT
+/////////////////////////////*/
+	Template.boLayout.events({
+		'click .logout':function(event){	
+			event.preventDefault();
+			Meteor.logout();
+		}
+	});
+
+/*//////////////////////////////
+     LOGIN
+/////////////////////////////*/	
 	Template.boLogin.events({
 		'submit form':function(event,template){
 			event.preventDefault();
@@ -32,10 +50,55 @@
 		}
 	});
 
-	Template.boLayout.events({
-		'click .logout':function(event){	
-			event.preventDefault();
-			Meteor.logout();
-			//Router.go('/')
+
+/*//////////////////////////////
+     BRANCH
+/////////////////////////////*/
+	Template.boAddBranch.events({
+		'click .save':function(evt,tmpl){
+			var name = tmpl.find('.branch-name').value;
+			var location = tmpl.find('.branch-location').value;
+			var active = tmpl.find('.branch-active').value;
+			console.log(name+location+active);
+			addBranch(name,location,active);
+			Session.set('showBoAddBranch',false);
+		},
+
+		'click .cancel':function(evt,tmpl){
+			Session.set('showBoAddBranch',false);
 		}
 	});
+
+	Template.boBranchList.events({
+		'click .add-branch':function(evt,tmpl){
+			Session.set('showBoAddBranch',true);
+		},
+
+		'click .branchItem':function(evt,tmpl){
+			Session.set('branchId',$(evt.target).closest('tr').data('id'));
+		}
+	});
+
+
+	Template.boBranches.showBranchDetails=function(id){
+		return Session.get('branchId');
+	}
+
+	Template.boBranchList.branchList = function(){
+		return Branches.find();
+	}
+
+	Template.boBranchList.showBoAddBranch = function(){
+		return Session.get('showBoAddBranch');
+	}
+
+	Template.boBranchDetails.branch = function(){
+		return Branches.findOne({_id:Session.get('branchId')});
+	}
+
+	var addBranch = function(name,location,active){
+		Branches.insert({name:name,location:location,active:active});	
+	}
+
+
+
--- collections.js
+++ collections.js
@@ -1,4 +1,6 @@
+//http://matteodem.github.io/meteor-easy-search/
 Queues = new Meteor.Collection("queues");
 Branches = new Meteor.Collection("branches");
+Branches.initEasySearch('name');
 Tickets = new Meteor.Collection("tickets");
 /*hello world*/
