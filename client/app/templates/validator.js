fff7f7235e0f4b7851be4ae017f8a8098023ec73
stas <stas.shwarzman@gmail.com>
2015-05-17 14:52:54 -0700

    leave queue, postpone queue, addtional details(without some validations)

---
+++ client/app/templates/validator.js
@@ -0,0 +1,46 @@
+inputFocus = function(input) {
+	$("#message").empty();
+	//input.removeClass("isInvalid");
+	//input.removeClass("isValid");
+}
+
+isValid = function() {
+	var invalids = 0;
+	$("#message").empty();
+
+	$(".appEmail").each(function() {
+		if (!isValidEmailAddress($(this).val())) {
+			invalids++;
+			$(this).removeClass("isValid");
+			$(this).addClass("isInvalid");
+            $("#message").append("error validating email \n");
+		} else {
+			$(this).removeClass("isInvalid");
+			$(this).addClass("isValid");
+		}
+	});
+
+	$(".appId").each(function() {
+		if (!isValidId($(this).val())) {
+			invalids++;
+			$(this).removeClass("isValid");
+			$(this).addClass("isInvalid");
+            $("#message").append("error validating id \n");
+		} else {
+			$(this).removeClass("isInvalid");
+			$(this).addClass("isValid");
+		}
+	});
+
+	return (invalids === 0);
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+
+function isValidId(id) {
+	var pattern = new RegExp(/^\d{8,9}$/);
+	return pattern.test(id);
+};
--- server/queuefunctions.js
+++ server/queuefunctions.js
@@ -1,34 +1,85 @@
 Meteor.methods({
-	addUserToQueue: function (phone, queueId) {
-		// 1. Check if queue exists
-		// 2	 If not - return error
-		// 3. Check if queue is open
-		// 4. 	If not - return error
-		// 5. Check if there are additional details
-		// 6. ask them..
-		// 5. Add user to queue, return his current location and queue current location 
-
-
-		// ----------------------------
-		console.log('server and parameters are (' + phone + ') and (' + queueId + ')');
+	getQueuesByBranch: function (branchId) {
+		console.log('getQueuesByBranch: server and parameters are (' + branchId + ')');
+		return Queues.find({branchId: branchId}).fetch();
+	},
+	getQueueAdditionalDetails: function (queueId) {
+		console.log('getQueueAdditionalDetails: server and parameters are (' + queueId + ')');
 
 		var queue = Queues.findOne(new Meteor.Collection.ObjectID(queueId));
 
 		if (queue === undefined || queue === null) {
-			console.log('cant find queue because it is ' + queue);
-			// error
+			console.log('no such queue');
 		} else {
-			var current = queue.current;
-			var next = queue.last + 1;
+			return queue.additionDetails;
+		}
+	},
+	addUserToQueue: addUser,
+	removeUserFromQueue: function (phone, queueId) {
+		removeUser(phone, queueId, "left");
+	},
+	postponeTurn: function (phone, queueId) {
+		console.log('postponeTurn: server and parameters are (' + phone + ') and (' + queueId + ')');
+		// leave queue
+		// join again	
 
-			Tickets.insert({phone: phone, 
-					sequence: next, 
-					queueId: queueId, 
-					creationTime: new Date().toTimeString(),
-					status: "waiting"
-			});
+		var ticket = Tickets.findOne({phone: phone, queueId: queueId.toHexString()});
 
-			Queues.update(new Meteor.Collection.ObjectID(queueId), {$set: {last: next}});
-		}		
+		if (ticket === undefined || ticket === null) {
+			console.log('no such ticket');
+		} else {
+			removeUser(phone, queueId, "passed");
+			addUser(phone, queueId.toHexString(), ticket.additionalDetails);
+		}
 	}
 }); 
+
+function removeUser(phone, queueId, status) {
+	// 1. TODO:Remove from tickets
+	// 2. Add to statistics
+	// 3. TODO:when the pakid clicks 'next', it should give the next AVAILABLE (not just ++)
+
+	console.log('removeUserFromQueue: server and parameters are (' + phone + ') and (' + queueId + ')');
+
+	var ticket = Tickets.findOne({phone: phone, queueId: queueId.toHexString()});
+
+	if (ticket === undefined || ticket === null) {
+		console.log('no such ticket');
+	} else {
+		console.log('removeUserFromQueue: found');
+		Tickets.remove({phone: phone, queueId: queueId.toHexString()});
+	}	
+}
+
+function addUser(phone, queueId, additionalDetails) {
+	console.log('addUserToQueue: server and parameters are (' + phone + ') and (' + queueId + ')');
+
+	var queue = Queues.findOne(new Meteor.Collection.ObjectID(queueId));
+
+	if (queue === undefined || queue === null) {
+		console.log('no such queue');
+	} else {
+		var next = queue.last;
+		var lastNum = parseInt(next.match(/\d+/)[0]) + 1;
+		next = next.replace(/(\d+)/g, lastNum);
+
+		if (additionalDetails != null) {
+			Tickets.insert({phone: phone, 
+				sequence: next, 
+				queueId: queueId, 
+				creationTime: new Date().toTimeString(),
+				status: "waiting",
+				additionalDetails: additionalDetails
+			});
+		} else {
+			Tickets.insert({phone: phone, 
+				sequence: next, 
+				queueId: queueId, 
+				creationTime: new Date().toTimeString(),
+				status: "waiting"
+			});
+		}
+
+		Queues.update(new Meteor.Collection.ObjectID(queueId), {$set: {last: next}});
+	}
+}
