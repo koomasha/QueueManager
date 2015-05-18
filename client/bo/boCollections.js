7d1fda1bee09d2434d87ee0d9c392485dc7438e8
koomasha <koomasha@gmail.com>
2015-05-18 22:32:44 +0300

    test

--- client/bo/boCollections.js
+++ client/bo/boCollections.js
@@ -1,13 +1,11 @@
 if(!Meteor.isCordova)
 {
-	Meteor.subscribe("Companies");
-	Meteor.subscribe("Queues",Session.get("branchId"));
-	Meteor.subscribe("Branches");
-	Meteor.subscribe("Tickets");
-
  	Tracker.autorun(function () {
 	  	Meteor.subscribe("boUsersInBranch", Session.get("branchId"));
 	  	Meteor.subscribe("boUsersByEmail", Session.get("userSearchString"),Session.get("branchId"));
+	  	Meteor.subscribe("Queues",Session.get("branchId"));
+	  	Meteor.subscribe("Branches");
+		Meteor.subscribe("Tickets");
 	});
 
 }
