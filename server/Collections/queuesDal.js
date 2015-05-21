06f8aafb189e205c24433dde885557149d1e4c50
koomasha <koomasha@gmail.com>
2015-05-21 03:52:48 +0300

    test

--- server/Collections/queuesDal.js
+++ server/Collections/queuesDal.js
@@ -5,12 +5,19 @@ Meteor.publish("Queues", function (branchid) {
 });
 
 
-/*
+
 Queues.allow({
-  insert: function (userId, branch) {return true;},
-  update: function (userId, doc, fieldNames, modifier){return true},
+  insert: function (userId, doc) {
+  	return GetAllowBranches(userId,doc.branchid,['Admin','Manager']) ;
+  },
+  update: function (userId, doc, fieldNames, modifier){
+  	return GetAllowBranches(userId,doc.branchid,['Admin','Manager']) ;
+  },
+  remove: function(userId, doc){
+  	return GetAllowBranches(userId,doc.branchid,['Admin','Manager']) ;
+  },
 });
-*/
+
 
 
 Queues.before.insert(function (userId, doc) {
@@ -18,6 +25,8 @@ Queues.before.insert(function (userId, doc) {
   doc.last = 0;
   doc.currentSec = 0;
   doc.opentickets = 0;
+  doc.additionalDetails = [];
+  doc.prefix='';
 });
 
 Meteor.methods({
@@ -26,13 +35,8 @@ Meteor.methods({
 			query: { _id: queueId },
 			update: { $inc: { currentSec: 1,opentickets: -1 }},
 			new: true
-		});
+		});	
 		var ticket = Tickets.findOne({queueId:queueId,sequence:queue.currentSec,status:'Waiting'});
 		return boChangeTicketStatus(ticket,"Getting Service",this.userId,'');
 	},
-	boSaveNewQueue:function(name,showtoclerk,active,prefix,branchid){
-		Queues.insert({name:name,showtoclerk:showtoclerk,active:active,prefix:prefix,branchid:branchid});
-	},
-
 });
-
