86c5d266c89b2c7831030181e15ddc02eb0f7676
SoulManSt <stas.shwarzman@gmail.com>
2015-05-23 07:02:18 -0700

    merge fixes

--- client/app/appCollections.js
+++ client/app/appCollections.js
@@ -1,8 +1,23 @@
 if(Meteor.isCordova)
 {
+	Meteor.startup(function () {
+		window.plugins.uniqueDeviceID.get(
+			function (result) {
+				console.log("phone id is: " + result);
+				Session.set('phoneid', result);
+
+				if(result){
+					BeforeTicket = new Meteor.Collection("BeforeTicket");
+					Meteor.subscribe('appTickets',result);
+				}
+
+			},
+			function () {
+				alert("Operation failed. Please restart the application.");
+			}
+		);
+	});
+
 	//TODO 
-	if(phoneId){
-		BeforeTicket = new Meteor.Collection("BeforeTicket");
-		Meteor.subscribe('appTickets',phoneId);
-	}
+
 }
