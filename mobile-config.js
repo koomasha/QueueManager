2c723d342396a01f5ff2c7daaeaae2fd8e74a206
olgasadna <olgayer.work@gmail.com>
2015-06-05 00:32:56 +0300

    added app logos

--- mobile-config.js
+++ mobile-config.js
@@ -1,1 +1,14 @@
 App.accessRule("*");
+
+App.info({
+  name: 'LineApp',
+  description: 'LineApp description',
+  version: '1.0.0'
+});
+
+App.icons({
+  'android_ldpi': 'public/mdpi_ic_launcher.png',
+  'android_mdpi': 'public/mdpi_ic_launcher.png',
+  'android_hdpi': 'public/hdpi_ic_launcher.png',
+  'android_xhdpi': 'public/xhdpi_ic_launcher.png'
+});
